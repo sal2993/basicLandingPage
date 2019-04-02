816d653bc26fcdd55df670936ba709f2dc44a694
sal camara <sal2993@hotmail.com>
2019-04-02 17:51:40 -0600

    Changed port to 8080

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const router = express.Router()
-const port = 80
+const port = 8080
 
 router.get('/health', (req, res) => res.send('Hello World!'))
 
